8ae84c8f99334f73bc145af38de027f6a62c3e2e
Samantha Dickeson <samlucydickeson@gmail.com>
2019-05-28 14:01:08 +0100

    add proper she-ra site data

--- src/cardData.js
+++ src/cardData.js
@@ -1,14 +1,15 @@
 import smoothieBar from "./assets/smoothie-bar.png";
 import jsCalculator from "./assets/calculator.png";
 import toDoApp from "./assets/to-do.png";
+import sheRa from "./assets/she-ra.png";
 
 const cardData = [
   {
     url: "https://github.com/starsuit/she-ra-character-selector",
-    image: smoothieBar,
+    image: sheRa,
     alt: "she-ra app",
     title: "She-Ra Character Selector",
-    description: "a description"
+    description: "My first React App! Select from a grid of characters."
   },
   {
     url: "https://github.com/FAC-Sixteen/Smoothie-bar",
@@ -16,21 +17,21 @@ const cardData = [
     alt: "smoothie bar app",
     title: "Smoothie Bar",
     description:
-      "A smoothie ordering app. Uses Express, Handlebars and PostgreSQL"
+      "A smoothie ordering app. Uses Express, Handlebars and PostgreSQL."
   },
   {
     url: "https://github.com/starsuit/calculator",
     image: jsCalculator,
     alt: "calculator app",
     title: "Calculator",
-    description: "A vanilla JS calculator"
+    description: "A vanilla JS calculator."
   },
   {
     url: "https://github.com/starsuit/FAC16-todo-app",
     image: toDoApp,
     alt: "to-do app",
     title: "To-Do App",
-    description: "A to do app using pure functions and test-driven development"
+    description: "A to do app using pure functions and test-driven development."
   }
 ];
 
