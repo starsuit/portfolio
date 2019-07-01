5c59e32d93c5d083babfbbd86963850b313424df
Samantha Dickeson <samlucydickeson@gmail.com>
2019-07-01 16:37:15 +0100

    update cardContainer to have a little intro

--- src/CardContainer.js
+++ src/CardContainer.js
@@ -7,20 +7,25 @@ const CardContainer = props => {
   const leftovers = cardData.length % 3;
   console.log(cardData.length, leftovers);
   return (
-    <ul className="card-container">
-      {cardData.map(data => (
-        <Card data={data} key={data.title} />
-      ))}
-      {leftovers > 0 &&
-        (leftovers === 2 ? (
-          <DarkCard />
-        ) : (
-          <React.Fragment>
-            <DarkCard />
-            <DarkCard />
-          </React.Fragment>
+    <React.Fragment>
+      <p className="look">
+        Please take a look at some of my favourite projects:
+      </p>
+      <ul className="card-container">
+        {cardData.map(data => (
+          <Card data={data} key={data.title} />
         ))}
-    </ul>
+        {leftovers > 0 &&
+          (leftovers === 2 ? (
+            <DarkCard />
+          ) : (
+            <React.Fragment>
+              <DarkCard />
+              <DarkCard />
+            </React.Fragment>
+          ))}
+      </ul>
+    </React.Fragment>
   );
 };
 
